1673a49fb4a3a5d163b5187a661fa45b150d55cb
Daniil Tihomirov <daniltihomirov946@gmail.com>
2024-08-10 14:13:37 +0300

    start

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    static Scanner cin = new Scanner(System.in);
+    public static void task1(){
+        System.out.println("напишите ваш возраст");
+        int age = cin.nextInt();
+        if (age >= 18){
+            System.out.println("Ты совершеннолетний");
+        } else {
+            System.out.println("тебе нет 18");
+        }
+
+
+    }
     public static void main(String[] args) {
+        task1();
 
     }
 }
