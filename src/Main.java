db6c9e5cf456fa7191e140a398a58b375ff09d9f
admin <yyyy@gmail.com>
2024-08-12 14:02:09 +0300

    end

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
+        String firstName = "Иван";
+        String middleName = "Ивановичь";
+        String lastName = "Иванов";
+        String fullName = "Иванёв Иван Ивановичь";
+        fullName = fullName.replace("ё", "е");
+        System.out.printf("Ф. И. О. сотрудника — %s\n", fullName);
+        String upFullName = fullName.toUpperCase();
+        System.out.printf("Данные Ф. И. О. сотрудника для заполнения отчета — %s", upFullName);
     }
 }
