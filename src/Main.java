35fa73d72764358f8729cfa94f4a724908074ce7
Med771 <mm7737333@gmail.com>
2024-09-17 12:01:51 +0300

    edit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         String firstName = "Семён";
         String middleName = "Семёнович";
         String lastName = "Иванов";
-        String fullName = String.format("%s %s %s", lastName, firstName, middleName);
+        String fullName =  lastName + " " + firstName + " " + middleName;
         fullName = fullName.replace("ё", "е");
         System.out.printf("Ф. И. О. сотрудника — %s\n", fullName);
         String upFullName = fullName.toUpperCase();
