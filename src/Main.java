9dbf5ce1ba82a9b12faf72df44de7a37e4ee14e0
Med771 <mm7737333@gmail.com>
2024-09-17 11:52:48 +0300

    edit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 public class Main {
-    public static boolean isLeap(short year){
-        if(year % 400 == 0){
+    public static boolean isLeap(short year) {
+        if (year % 400 == 0) {
             return true;
         }
         if (year % 100 == 0) {
@@ -10,40 +10,43 @@ public class Main {
         }
         return year % 4 == 0;
     }
-    public static void phoneApp(byte os, short year, short correctYear){
-        if (os == 1){
-            if (year >= correctYear){
-                System.out.println("скачай обычную версию на Android");
-            }else {
-                System.out.println("скачай облегченую версию на Android");
-            }
-        } else {
-            if (year >= correctYear){
-                System.out.println("скачай обычную версию на IOS");
-            } else {
-                System.out.println("скачай облегченую версию на IOS");
-            }
+
+    public static void phoneApp(byte os, short year, short correctYear) {
+        if (os == 1 && year < correctYear) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+        if (os == 0 && year < correctYear) {
+            System.out.println("становите облегченную версию приложения для iOS по ссылке");
+        }
+        if (os == 0 && year > correctYear) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        if (os == 1 && year > correctYear) {
+            System.out.println("Установите версию приложения для Android по ссылке");
         }
     }
-    public static int deliveryTime(short distance){
-        if (distance < 20){
+
+    public static int deliveryTime(short distance) {
+        if (distance < 20) {
             return 1;
         }
-        if (distance > 100){
+        if (distance > 100) {
             return 0;
         }
-        if (distance > 60){
-            return 4;
+        if (distance > 60) {
+            return 3;
         }
         return 2;
 
     }
+
     public static Scanner cin = new Scanner(System.in);
+
     public static void main(String[] args) {
         short year = cin.nextShort();
-        if (isLeap(year)){
+        if (isLeap(year)) {
             System.out.println("високосный год");
-        }else {
+        } else {
             System.out.println("невисокосный год");
         }
 
